65abb45df4a60ca7575453c4b83084b2197fd03d
Maxim Ermakov <ermakov@steelkiwi.com>
2018-10-29 16:12:56 +0200

    rxjava example

--- app/src/main/java/com/swat_cat/firstapp/screens/movie_search/MovieSearchPresenter.java
+++ app/src/main/java/com/swat_cat/firstapp/screens/movie_search/MovieSearchPresenter.java
@@ -8,10 +8,13 @@ import com.swat_cat.firstapp.services.rest.dto.SearchResultDTO;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 
 public class MovieSearchPresenter implements MovieSearchContract.Presenter {
 
@@ -27,6 +30,49 @@ public class MovieSearchPresenter implements MovieSearchContract.Presenter {
     public void start(MovieSearchContract.View view) {
         this.view = view;
         repository = new MovieRepositoryImpl();
+        subscriptions.add(
+                view.searchChanged()
+                        .debounce(400,TimeUnit.MILLISECONDS)
+                        .skip(1)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .doOnNext(new Consumer<CharSequence>() {
+                            @Override
+                            public void accept(CharSequence charSequence) throws Exception {
+                                view.showLoading(true);
+                            }
+                        })
+                        .flatMap(new Function<CharSequence, ObservableSource<SearchResultDTO>>() {
+                            @Override
+                            public ObservableSource<SearchResultDTO> apply(CharSequence charSequence) throws Exception {
+                                String query= charSequence.toString().trim();
+                                return repository.search(query);
+                            }
+                        })
+                        .subscribe(
+                                new Consumer<SearchResultDTO>() {
+                                    @Override
+                                    public void accept(SearchResultDTO searchResultDTO) throws Exception {
+                                        view.showLoading(false);
+                                        if (searchResultDTO != null) {
+                                            List<SearchItemDTO> list = searchResultDTO.getSearch();
+                                            if (list != null && !list.isEmpty()) {
+                                                view.showEmpty(false);
+                                                view.setMovieList(list);
+                                                view.showList(true);
+                                            } else {
+                                                view.showEmpty(true);
+                                                view.showList(false);
+                                            }
+
+                                        } else {
+                                            view.showList(false);
+                                            view.showEmpty(true);
+
+                                        }
+                                    }
+                                }
+                        ));
+
         view.searchChanged()
                 .debounce(400, TimeUnit.MILLISECONDS)
                 .skip(1)
